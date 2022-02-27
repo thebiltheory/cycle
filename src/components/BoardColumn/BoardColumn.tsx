de40df5295aeae235183321cf0fcb6e9c99d5855
Bil Benhamou <bil.benhamou@gmail.com>
2022-02-27 17:08:22 +0100

    feat: added max height to columns

--- src/components/BoardColumn/BoardColumn.tsx
+++ src/components/BoardColumn/BoardColumn.tsx
@@ -29,6 +29,7 @@ const BoardColumn: FC<any> = ({ column, tickets, index, onClickNewDoc }) => {
           borderRadius={2}
           width={isCollapsed ? 33 : '270px'}
           height={isCollapsed ? '100%' : 'fit-content'}
+          maxHeight="90vh"
           pb={2}
           // sx={{
           //   willChange: 'width',
